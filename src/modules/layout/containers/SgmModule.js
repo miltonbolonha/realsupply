49e413f767e414ba624efcf7f33e5c7e338b1a63
miltonbolonha <miltonbolonha@gmail.com>
2021-08-08 12:17:42 -0300

    Bugfix: es5 module

--- src/modules/layout/containers/SgmModule.js
+++ src/modules/layout/containers/SgmModule.js
@@ -1,9 +1,9 @@
 // import fetch from 'node-fetch'
-const fetch = require('node-fetch')
+const { fetch } = require('node-fetch')
 
 async function SgmModule(params) {
 	return (
-		console.log('... VAAAIIIII ... SendGrid Masterfullish'),
+		console.log(`...${params} VAAAIIIII ... SendGrid Masterfullish`),
 		fetch(`/.netlify/functions/sgm?action=${params}`)
 			.then((res) => res.text())
 			.then((text) => console.log(text))
